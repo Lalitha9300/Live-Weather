6b8175147d7e282de1754fb72bb2be78ff9e7b9a
Lalitha9300 <lalitha9300@gmail.com>
2024-04-22 15:37:23 +0530

    Adding code and assets

---
+++ script.js
@@ -0,0 +1,46 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+
+search.addEventListener('click',()=>{
+    const APIKey ='a0bc8605d7f642ab9f6552b1f38aaef3';
+    const city=document.querySelector('.search-box input').value;
+
+    if(city=='')
+    return;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then(json => {
+        
+        const image = document.querySelector('.weather-box img');
+        const temperature = document.querySelector('.weather-box .temperature');
+        const description = document.querySelector('.weather-box .description');
+        const humidity = document.querySelector('.weather-details .humidity span');
+        const wind = document.querySelector('.weather-details .wind span');
+
+        switch (json.weather[0].main) {
+            case 'Clear':
+                image.src = "assets/clear.png";
+                break;
+            case 'Rain':
+                image.src = "assets/rain.png";
+                break;
+            case 'Snow':
+                image.src = "assets/snow.png";
+                break;
+            case 'Clouds':
+                image.src = "assets/cloud.png";
+                break; 
+            case 'Mist':
+                image.src = "assets/mist.png";
+                break;
+            case 'Haze':
+                image.src = "assets/mist.png";
+                break;      
+            default:
+                image.src = 'assets/cloud.png';
+        }
+    })
+})
+
+
